1e87cdfc4e2b3fdb719cad252b9c5402e6dfb838
Abhiram <abhiramtripathipanna@gmail.com>
2023-03-17 20:06:22 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,91 @@
+const express= require("express")
+require('./db/config');
+const cors=require("cors");
+const User= require("./db/User");
+const Product=require("./db/Product");
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.post("/register",async (req,res)=>{
+    let user=new User(req.body);
+    let result= await user.save();
+    res.send(result);
+})
+
+app.post("/login",async (req,res)=>{
+    if(req.body.password && req.body.email)
+    {
+        let user=await User.findOne(req.body).select("-password");
+        if(user){
+            res.send(user);
+        } else{
+            res.send({result: "No user found"})
+        }
+    }
+    else{
+        res.send({result: 'Complete Input'})
+    }
+    })
+app.post("/add-product",async (req,res)=>{
+    let product=new Product(req.body); 
+    let result=await product.save();
+    res.send(result);
+})
+
+app.get("/products",async(req,res)=>{
+    const products= await Product.find();
+    if(products.length>0)
+    {
+        res.send(products);
+    }
+    else
+    {
+        res.send({result: "No Product"})
+    }
+})
+
+app.delete("/product/:id",async(req,res)=>{
+    let result=await Product.deleteOne({_id:req.params.id});
+    res.send(result);
+})
+
+app.get("/product/:id",async(req,res)=>{
+    let result=await Product.findOne({_id:req.params.id})
+    if(result){
+        res.send(result);
+    }
+    else
+    {
+        res.send({"result":"NO"})
+    }
+})
+
+app.put("/product/:id",async (req,res)=>{
+    let result=await Product.updateOne(
+        {_id:req.params.id},
+        {
+            $set: req.body
+        }
+       
+    )
+    res.send(result);
+})
+
+app.get("/search/:key", async(req,res)=>{
+    let result=await Product.find({
+        "$or": [
+            {
+                name: { $regex: req.params.key}
+                
+            },
+            {
+                company: {$regex :req.params.key}
+            }
+        ] 
+    });
+    res.send(result);
+})
+
+app.listen(8000,()=>{
+    console.log("Server is on with port 8000")
+})
